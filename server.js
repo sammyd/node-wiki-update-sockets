7f509930cacbfbf6939f3e5a9160dc679fb9cdf8
Sam Davies <sdavies@shinobicontrols.com>
2014-04-29 13:50:56 +0100

    Add some comments and remove the static file serving
    We only want to serve the web sockets.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Prepare some imports
 var WebSocketServer = require('ws').Server,
     http = require('http'),
     express = require('express'),
@@ -6,22 +7,23 @@ var WebSocketServer = require('ws').Server,
     _ = require('underscore'),
     irc = require('irc');
 
-
-app.use(express.static(__dirname + '/'));
-
+// Create the http server which will serve the websocket requests
 var server = http.createServer(app);
 server.listen(port);
-
 console.log('http server listening on %d', port);
 
+// This stores the active web socket clients
 var clients = [];
 
+// Create a websocket server
 var wss = new WebSocketServer({server: server});
 console.log('WebSocketServer created');
 wss.on('connection', function(ws) {
+    // Add the active websocket client to the clients collection
     clients.push(ws);
     console.log('websocket %d connection open', clients.length);
 
+    // When we close the connection, remove it from the clients collection
     ws.on('close', function() {
         console.log("websocket connection close");
         console.log("websocket index: %d", clients.indexOf(ws));
@@ -30,14 +32,17 @@ wss.on('connection', function(ws) {
     });
 });
 
+// Create an IRC client and connect the the update channel on Wikipedia-EN
 var ircClient = new irc.Client('irc.wikimedia.org', 'wiki-update-sockets', {
     channels: ['#en.wikipedia'],
 });
 
+// Handle errors just by logging them
 ircClient.addListener('error', function(message) {
     console.log('error: ', message);
 });
 
+// Messages should be processed and sent to each of the web socket clients
 ircClient.addListener('message', function(from, to, message) {
     _.each(clients, function(client, index, list) {
         client.send("Message received: " + message);
